e5fe7691f9dd96309337bc181897ded94fa5d6ed
Shubhangi Sarode <88708073+sarodeshubhangi@users.noreply.github.com>
2023-05-09 15:21:32 +0200

    Add files via upload

---
+++ Hurdle 4.py
@@ -0,0 +1,35 @@
+def turn_arround():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_arround()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move() 
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+    
+    
+
+
+
+
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
