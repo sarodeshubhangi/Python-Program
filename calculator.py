875f75a9d98e430c6527c46c69e3c969cc8814df
Shubhangi Sarode <88708073+sarodeshubhangi@users.noreply.github.com>
2023-07-06 15:19:14 +0200

    Add files via upload
    Calculator using functions

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# Calculator
+# Addition
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+}
+num1 = int(input("What's the first number? "))
+num2 = int(input("What's the second number? "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Choose the operations from above line: ")
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+
+
+
+
+
